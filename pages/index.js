127f2b4b730949867e1b3a599e333aa84dabceb4
ramprits <rampritsahani@outlook.in>
2019-01-27 11:09:59 +0530

    Navbard adds

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-
+import { Button, Alert } from "reactstrap";
+import Layout from "../components/Layout";
 export default class index extends Component {
   render() {
     return (
       <React.Fragment>
-        <h2 style={{ textAlign: "center" }}>
-          Welcome to Nextjs application worls!
-        </h2>
+        <Layout>
+          <h2 style={{ textAlign: "center" }}>
+            Welcome to Nextjs application worls!
+          </h2>
+          <Alert color="primary">
+            Hello Project is strapi-next with Bootstrap
+          </Alert>
+          &nbsp; <Button color="primary">Hello from nextjs</Button>
+        </Layout>
       </React.Fragment>
     );
   }
