6a0504a21df1be5cdcb0e0d59f5dbc943d8762ff
ramprits <rampritsahani@outlook.in>
2019-01-27 10:48:11 +0530

    My first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class index extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2 style={{ textAlign: "center" }}>
+          Welcome to Nextjs application worls!
+        </h2>
+      </React.Fragment>
+    );
+  }
+}
